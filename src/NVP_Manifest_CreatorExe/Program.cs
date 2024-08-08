8bbe7de607fe7771e7ca7a3724fb98400356dcee
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2024-08-08 22:02:14 +0300

    upd manifest creator
    Упрощение методики создания файла манифеста, сохранение guids_map теперь в исходники. Генерация остальных элементов по правилам

--- src/NVP_Manifest_CreatorExe/Program.cs
+++ src/NVP_Manifest_CreatorExe/Program.cs
@@ -3,18 +3,80 @@ using System;
 using System.Xml.Linq;
 using System.IO;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 
 namespace NVP_Manifest_Creator
 {
+    public class Auxiliary_Guids_Map_Item
+    {
+        public string Path { get; set; }
+        public string Id { get; set; }
+    }
+    public class Auxiliary_Guids_Map
+    {
+        [XmlIgnore]
+        public Dictionary<string, string> ElementsMap2 { get; set; }
 
+        public List<Auxiliary_Guids_Map_Item> ElementsMap;
+
+        public Auxiliary_Guids_Map()
+        {
+            this.ElementsMap = new List<Auxiliary_Guids_Map_Item>();
+            this.ElementsMap2 = new Dictionary<string, string>();
+        }
+
+        public static Auxiliary_Guids_Map LoadFromFile(string filePath)
+        {
+            if (!File.Exists(filePath)) return new Auxiliary_Guids_Map();
+
+            Auxiliary_Guids_Map data;
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Auxiliary_Guids_Map));
+
+            using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate))
+            {
+                data = xmlSerializer.Deserialize(fs) as Auxiliary_Guids_Map;
+            }
+
+            foreach (var item in data.ElementsMap)
+            {
+                data.ElementsMap2.Add(item.Path, item.Id);
+            }
+
+            return data;
+        }
+
+        public void Save(string savePath)
+        {
+            foreach (var item in ElementsMap2)
+            {
+                Auxiliary_Guids_Map_Item def = new Auxiliary_Guids_Map_Item();
+                def.Path = item.Key;
+                def.Id = item.Value;
+                ElementsMap.Add(def);
+            }
+            XmlSerializer formatter = new XmlSerializer(typeof(Auxiliary_Guids_Map));
+            // сохранение массива в файл
+            using (FileStream fs = new FileStream(savePath, FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, this);
+            }
+        }
+    }
     public class AppExe
     {
+        private const string _CoderName = "GeorgGrebenyuk";
         public static void Main(string[] args)
         {
+            string sade_directory = AppDomain.CurrentDomain.BaseDirectory;
+            string root_repo_path = new DirectoryInfo(sade_directory).Parent.Parent.Parent.FullName;
             foreach (string dll_path in Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll", SearchOption.TopDirectoryOnly))
             {
                 bool is_any_nodes = false;
                 //List<NVP_Manifest> nodeitems = new List<NVP_Manifest>();
+                string dll_name = Path.GetFileNameWithoutExtension(dll_path);
+                string dll_nameDLL = Path.GetFileName(dll_path);
+                string guids_map_path = Path.Combine(root_repo_path, "src", dll_name, dll_name + "_guids_map.xml");
+                Auxiliary_Guids_Map guids_map = Auxiliary_Guids_Map.LoadFromFile(guids_map_path);
 
                 Dictionary<string, List<NVP_Manifest>> nodeitems = new Dictionary<string, List<NVP_Manifest>>();
 
@@ -44,10 +106,37 @@ namespace NVP_Manifest_Creator
 
                         //get fill path
                         string cs_path = type.Namespace;
+                        string nvp_node_path = type.Namespace + "." + type.Name;
                         if (cs_path.Contains("."))
                         {
                             cs_path = cs_path.Substring(0, cs_path.IndexOf("."));
                         }
+
+
+                        NVP_Manifest_attrs_need.PathAssembly = dll_nameDLL;
+                        NVP_Manifest_attrs_need.PathExecuteClass = nvp_node_path;
+                        NVP_Manifest_attrs_need.Folder = type.Namespace;
+                        NVP_Manifest_attrs_need.CoderName = _CoderName;
+
+                        if (guids_map.ElementsMap2.ContainsKey(nvp_node_path)) NVP_Manifest_attrs_need.Id = guids_map.ElementsMap2[nvp_node_path];
+                        else
+                        {
+                            if (NVP_Manifest_attrs_need.Id != null) 
+                            {
+                                guids_map.ElementsMap2.Add(nvp_node_path, NVP_Manifest_attrs_need.Id);
+                            }
+                            else
+                            {
+                                NVP_Manifest_attrs_need.Id = Guid.NewGuid().ToString("D").ToUpper();
+                                guids_map.ElementsMap2.Add(nvp_node_path, NVP_Manifest_attrs_need.Id);
+                            }
+                        }
+
+                        if (NVP_Manifest_attrs_need.CADType == null) NVP_Manifest_attrs_need.CADType = "None";
+                        if (NVP_Manifest_attrs_need.NodeType == null) NVP_Manifest_attrs_need.NodeType = "Loaded";
+                        if (NVP_Manifest_attrs_need.ViewType == null) NVP_Manifest_attrs_need.ViewType = "Modifier";
+                        if (NVP_Manifest_attrs_need.Text == null) NVP_Manifest_attrs_need.Text = "";
+
 
                         if (nodeitems.ContainsKey(cs_path)) nodeitems[cs_path].Add(NVP_Manifest_attrs_need);
                         else nodeitems.Add(cs_path, new List<NVP_Manifest> { NVP_Manifest_attrs_need });
@@ -93,7 +182,8 @@ namespace NVP_Manifest_Creator
 
                         Console.WriteLine(nodeitemPath);
                     }
-                    
+
+                    guids_map.Save(guids_map_path);
                 }
             }
 
