eca55774174f9dda80e1a42954f036cf894bfd11
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2024-08-10 12:49:07 +0300

    upd output archives
    Имена архивы заменены: убрано упоминание ActiveX в связи с переименовыванием репозитория

--- Build_release.bat
+++ Build_release.bat
@@ -31,10 +31,10 @@ xcopy "bin\Release\net6.0\NVP_Renga_COM.*" "bin\Release\net6.0\NPV_Data\" /Y /I
 ::Copy all *.nodeitem
 xcopy "bin\Release\net6.0\*.nodeitem" "bin\Release\net6.0\NPV_Data\" /Y /I
 ::Move all data to ZIP in root of repository
-IF EXIST "NPV_NodeLibs_ActiveX_net6.0" del "NPV_NodeLibs_ActiveX_net6.0" /S /Q
-xcopy "bin\Release\net6.0\NPV_Data\*.*" "NPV_NodeLibs_ActiveX_net6.0\"  /Y /I
-"C:\Program Files\7-Zip\7z" a -tzip "NPV_NodeLibs_ActiveX_net6.0.zip" "NPV_NodeLibs_ActiveX_net6.0"
-rmdir "NPV_NodeLibs_ActiveX_net6.0" /S /Q
+IF EXIST "NPV_NodeLibs_net6.0" del "NPV_NodeLibs_net6.0" /S /Q
+xcopy "bin\Release\net6.0\NPV_Data\*.*" "NPV_NodeLibs_net6.0\"  /Y /I
+"C:\Program Files\7-Zip\7z" a -tzip "NPV_NodeLibs_net6.0.zip" "NPV_NodeLibs_net6.0"
+rmdir "NPV_NodeLibs_net6.0" /S /Q
 REM Step 3.2 For net48
 
 ::Copy the dll with our custom manifest
@@ -50,8 +50,8 @@ xcopy "bin\Release\net48\NVP_Renga_COM.*" "bin\Release\net48\NPV_Data\" /Y /I
 ::Copy all *.nodeitem
 xcopy "bin\Release\net48\*.nodeitem" "bin\Release\net48\NPV_Data\" /Y /I
 ::Move all data to ZIP in root of repository
-IF EXIST "NPV_NodeLibs_ActiveX_net48" del "NPV_NodeLibs_ActiveX_net48" /S /Q
-xcopy "bin\Release\net48\NPV_Data\*.*" "NPV_NodeLibs_ActiveX_net48\" /Y /I
-"C:\Program Files\7-Zip\7z" a -tzip "NPV_NodeLibs_ActiveX_net48.zip" "NPV_NodeLibs_ActiveX_net48"
-rmdir "NPV_NodeLibs_ActiveX_net48" /S /Q
+IF EXIST "NPV_NodeLibs_net48" del "NPV_NodeLibs_net48" /S /Q
+xcopy "bin\Release\net48\NPV_Data\*.*" "NPV_NodeLibs_net48\" /Y /I
+"C:\Program Files\7-Zip\7z" a -tzip "NPV_NodeLibs_net48.zip" "NPV_NodeLibs_net48"
+rmdir "NPV_NodeLibs_net48" /S /Q
 ::pause >nul
